be17a81dc835392b69d809a1526b7cd6b3dc4500
mbenitez <mbenitez.a@gmail.com>
2023-02-19 10:41:02 -0500

    Merge latest main commits with feature/auth branch

--- client/src/components/PageContainer.jsx
+++ client/src/components/PageContainer.jsx
@@ -1,4 +1,5 @@
-import { Routes, Route } from "react-router-dom";
+import { useState } from "react";
+import { Routes, Route, useNavigate } from "react-router-dom";
 import Header from './Header';
 import Footer from './Footer';
 import LandingPage from './Pages/landingPage';
@@ -7,23 +8,63 @@ import Wizard from './Wizard';
 import Homepage from './Pages/Homepage';
 import UploadImage from "./Pages/uploadPage";
 import ProjectPage from "./Pages/ProjectPage";
+import Protected from "./Protected";
+import { auth } from "../firebase";
+
 import "../styles/main.css"
 
-function PageContainer() {
+const PageContainer = () => {
+    const navigate = useNavigate();
+    const [isLoggedIn, setIsLoggedIn] = useState(null);;
 
+    const toLogin = () =>{
+        setIsLoggedIn(true);
+        PageContainer.setIsLoggedIn = true;
+        const loginBtn = document.querySelector('#login');
+        navigate('/login');
+        loginBtn.style.display = 'none';
+        //loginBtn.textContent= "Logout";
+    }
+
+    const toLogout = () =>{
+        setIsLoggedIn(false);
+        const loginBtn = document.querySelector('#login');
+        auth.signOut().then(function() {
+            // Sign-out successful.
+            console.log('User Logged Out!');
+          }).catch(function(error) {
+            // An error happened.
+            console.log(error);
+          });
+          //toLanding();
+        navigate('/');
+        //loginBtn.style.display = 'inline';
+    }
+
+    const logIn = () => {
+        setIsLoggedIn(true);
+    };
+    const logOut = () => {
+        setIsLoggedIn(false);
+    };
+        
     return (
-        <div className="pageContainer">
-            <Header />
+    <div className="pageContainer">
+        <Header />
+            { isLoggedIn
+                ? (<button className='myBtn' id='login' onClick={toLogout} style={{display: 'inline'}}>Logout</button>)
+                : (<button className='myBtn' id='login' onClick={toLogin} style={{display: 'inline'}}>Login/SignUp</button>)
+            }
             <Routes>
                 <Route path= "/" element={<LandingPage />} />
                 <Route path= "/login" element={<SignPage />} />
-                <Route path="/wizard" element={<Wizard />}/>
-                <Route path="/home" element={<Homepage />}/>
-                <Route path="/project/:id" element={<ProjectPage/>}/>
-                <Route path="/upload" element={<UploadImage/>}/>
+                <Route path="/wizard" element={<Protected isLoggedIn={isLoggedIn}><Wizard /></Protected>}/>
+                <Route path="/home" element={<Protected isLoggedIn={isLoggedIn}><Homepage /></Protected>}/>
+                <Route path="/project/:id" element={<Protected isLoggedIn={isLoggedIn}><ProjectPage/></Protected>}/>
+                <Route path="/upload" element={<Protected isLoggedIn={isLoggedIn}><UploadImage/></Protected>}/>
             </Routes>
-            <Footer />
-        </div>
+        <Footer />
+    </div>
     )
 }
 
