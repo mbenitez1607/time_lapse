0590ebff0361dde619d860adcf30b044a396597e
Samsonite <sam.demarco@gmail.com>
2023-02-23 16:15:29 -0500

    Fixed Upload directory issue for image storage

--- server/helpers/filehelper.js
+++ server/helpers/filehelper.js
@@ -1,16 +1,23 @@
 import multer from 'multer';
 import { v4 as uuidv4 } from 'uuid';
+import fs from 'fs';
+
 uuidv4();
 
 const storage = multer.diskStorage({
     destination: (req, file, cb) => {
-        cb(null, 'uploads');
+        const uploadDir = './uploads';
+        if (!fs.existsSync(uploadDir)){
+            fs.mkdirSync(uploadDir);
+        }
+        cb(null, uploadDir);
     },
     filename: (req, file, cb) => {
         const fileName = uuidv4() + '-' + file.originalname
-        cb(null,fileName);
+        cb(null, fileName);
     }
 });
+
 const filefilter = (req, file, cb) => {
     if (file.mimetype === 'image/png' || file.mimetype === 'image/jpg' 
         || file.mimetype === 'image/jpeg'){
